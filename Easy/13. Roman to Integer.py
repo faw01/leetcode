87ceb25c57fc8635cc4b2b180719c525d2f23cc9
faw <fawwad30@gmail.com>
2022-12-05 01:59:25 +0800

    Update 13. Roman to Integer.py

--- Easy/13. Roman to Integer.py
+++ Easy/13. Roman to Integer.py
@@ -1,4 +1,5 @@
 class Solution:
+    # first attempt - LEETCODE SUBMISSION: Accepted
     def romanToInt(self,s: str) -> int:
         roman_to_int_map = {
             'I': 1,
@@ -10,16 +11,41 @@ class Solution:
             'M': 1000
         }
         final_int = 0
-        s = s[::-1] # reverse
+        s = s[::-1] # reverse VICXMCM
         for char in s:
             print(char)
-            get_map_value = roman_to_int_map.get(char)
+            get_map_value = roman_to_int_map.get(char) 
             final_int += get_map_value
         print(f'int is: {final_int}')
         return final_int
 
+    # 
+
+    # neetcode attempt - LEETCODE SUBMISSION: Accepted
+    def romanToInt(self,s: str) -> int:
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X' : 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        res = 0
+
+        for i in range(len(s)):
+            print(i)
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                res -= roman[s[i]]
+            else:
+                res += roman[s[i]]
+        
+        return res
+
+
 if __name__ == "__main__":
     sol = Solution()
-    roman = "MCMXCIV"
-    assert sol.romanToInt(roman) == 1994
-
+    roman = "III"
+    assert sol.romanToInt(roman) == 3
